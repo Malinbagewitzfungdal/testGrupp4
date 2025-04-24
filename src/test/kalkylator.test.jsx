5f10c6f2309591a36a08052664b40bae2c3b3e62
Carlos Ibanez <carlos.ibanez@live.se>
2025-04-24 11:00:41 +0200

    la till describe

--- src/test/kalkylator.test.jsx
+++ src/test/kalkylator.test.jsx
@@ -3,15 +3,17 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from '../App';
 
-test('visar 0 från början', () => {
-  render(<App />);
-  const button = screen.getByRole('button', { name: /count is 0/i });
-  expect(button).toBeInTheDocument();
-});
+describe('Kalkylator App', () => {
+  test('visar 0 från början', () => {
+    render(<App />);
+    const button = screen.getByRole('button', { name: /count is 0/i });
+    expect(button).toBeInTheDocument();
+  });
 
-test('ökar med 1 efter ett klick', () => {
-  render(<App />);
-  const button = screen.getByRole('button', { name: /count is 0/i });
-  fireEvent.click(button);
-  expect(screen.getByRole('button', { name: /count is 1/i })).toBeInTheDocument();
+  test('ökar med 1 efter ett klick', () => {
+    render(<App />);
+    const button = screen.getByRole('button', { name: /count is 0/i });
+    fireEvent.click(button);
+    expect(screen.getByRole('button', { name: /count is 1/i })).toBeInTheDocument();
+  });
 });
